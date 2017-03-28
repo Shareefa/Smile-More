fd3941fa3aa36761614fa19ebace6f77693a7897
shareefa <abdullah.shareef@gmail.com>
2017-03-28 16:44:53 -0400

    Switched to C++ and tested

---
+++ CMakeFiles/faceDectect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/faceDectect.dir/faceDetect.cpp.o"
+  "faceDectect.pdb"
+  "faceDectect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/faceDectect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
